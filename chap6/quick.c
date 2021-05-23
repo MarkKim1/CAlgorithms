2d200779185af7f11e764218473afed2991790d9
MarkKim1 <kim970602@gmail.com>
2021-05-23 07:10:35 +0000

    C Algorithms

--- chap6/quick.c
+++ chap6/quick.c
@@ -1,43 +1,54 @@
 #include <stdio.h>
-#include <stdlib.>
+#include <stdlib.h>
 
 #define swap(type,x,y) do{ type t = x; x = y; y = t;}while(0)
 
-void quick(int a[], int left, int right)
+void quick(int a[], int n)
 {
+	int left = 0;
+	int right = n-1;
 	int pl = left;
 	int pr = right;
 	int x = a[(pl+pr) / 2];
+	
+	int i;
+	printf("a[%d] ~ a[%d]: {", left, right);
+	for(i = left; i < right; i++)
+	{
+		printf("%d", a[i]);
+	}
+	printf("%d}\n", a[right]);
+	
 	do{
 		while(a[pl] < x) pl++;
-		while(a[pr] > x) pr++;
+		while(a[pr] > x) pr--;
 		if(pl <= pr){
 			swap(int, a[pl], a[pr]);
 			pl++;
 			pr--;
 		}
 	}while(pl <= pr);
-	if(left < pr) quick(a,left, pr);
-	if(pl < right) quick(a, pl, right);
+	if(left < pr) quick(a,n-1-pr);
+	if(pl < right) quick(a,n-1-pl);
 }
 
-int main (){
-int i,nx;
-int *x;
-puts("퀵 정렬");
-printf("요소 개수");
-scanf("%d", &nx);
-x = calloc(nx,sizeof(int));
-for(i = 0; i < nx; i++)
-{
-	printf("x[%d]", i);
-	scanf("%d",&x[i]);
-}
-quick(x,0,nx-1);
+	int main (){
+	int i,nx;
+	int *x;
+	puts("퀵 정렬");
+	printf("요소 개수");
+	scanf("%d", &nx);
+	x = calloc(nx,sizeof(int));
+	for(i = 0; i < nx; i++)
+	{
+		printf("x[%d]", i);
+		scanf("%d",&x[i]);
+	}
+	quick(x,nx);
 	puts("오름차순으로 정렬했습니다.");
 	for(i = 0; i < nx; i++)
 	{
-		printf("x[%d] = %d", i, x[i]);
+		printf("x[%d] = %d\n", i, x[i]);
 	}
 	free(x);
 
---
+++ chap6/quick_nr.c
@@ -0,0 +1,63 @@
+void quick(int a[], int left, int right)
+{
+	IntStack lstack;
+	IntStack rstack;
+	
+	Initialize(&lstack, right-left+1);
+	Initialize(&rstack, right - left+1);
+	
+	Push(&lstack, left);
+	Push(&rstack, right);
+	
+	while(!isEmpty(&lstack))
+	{
+		int pl = (Pop(&lstack,&left), left);
+		int pr = (Pop(&rstack, &right), right);
+		int x = a[(left + right) / 2];
+		do{
+		while(a[pl] < x) pl++;
+		while(a[pr] > x) pr--;
+		if(pl <= pr)
+		{
+			swap(int, a[pl], a[pr]);
+			pl++;
+			pr--;
+			}
+		}while(pl <= pr);
+		
+		if(left < pr) {
+			Push(&lstack, left);
+			Push(&rstack, pr);
+		}
+		if(pl < right){
+			Push(&lstack, pl);
+			Push(&rstack, right);
+		}
+		Terminate(&lstack);
+		Terminate(&rstack);
+	}
+}
+
+int main (){
+	int i,nx;
+	int *x;
+	puts("퀵 정렬");
+	printf("요소 개수");
+	scanf("%d", &nx);
+	x = calloc(nx,sizeof(int));
+	for(i = 0; i < nx; i++)
+	{
+		printf("x[%d]", i);
+		scanf("%d",&x[i]);
+	}
+	quick(x,nx);
+	puts("오름차순으로 정렬했습니다.");
+	for(i = 0; i < nx; i++)
+	{
+		printf("x[%d] = %d\n", i, x[i]);
+	}
+	free(x);
+
+
+return 0;
+}
