da8dc3026aa99d4bde29fd3784016727d80c98a6
MarkKim1 <kim970602@gmail.com>
2021-05-23 02:08:00 +0000

    somethings

--- chap6/quick.c
+++ chap6/quick.c
@@ -1,19 +1,46 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define swap(type,x,y) do{type t=x; x=y; y=t;}while(0)
+#include <stdlib.>
 
-int main ()
+#define swap(type,x,y) do{ type t = x; x = y; y = t;}while(0)
+
+void quick(int a[], int left, int right)
 {
-    int i,nx;
-    int *x;
-    puts("퀵 정렬");
-    printf("요소 개수 :");
-    scanf("%d", &nx);
-    x = calloc(nx, sizeof(int));
-    for(i = 0; i < nx; i++)
-    {
-        printf("x[%d] :", i);
-        scanf("%d", &x[i]);
-    }
-    return 0;
+	int pl = left;
+	int pr = right;
+	int x = a[(pl+pr) / 2];
+	do{
+		while(a[pl] < x) pl++;
+		while(a[pr] > x) pr++;
+		if(pl <= pr){
+			swap(int, a[pl], a[pr]);
+			pl++;
+			pr--;
+		}
+	}while(pl <= pr);
+	if(left < pr) quick(a,left, pr);
+	if(pl < right) quick(a, pl, right);
+}
+
+int main (){
+int i,nx;
+int *x;
+puts("퀵 정렬");
+printf("요소 개수");
+scanf("%d", &nx);
+x = calloc(nx,sizeof(int));
+for(i = 0; i < nx; i++)
+{
+	printf("x[%d]", i);
+	scanf("%d",&x[i]);
+}
+quick(x,0,nx-1);
+	puts("오름차순으로 정렬했습니다.");
+	for(i = 0; i < nx; i++)
+	{
+		printf("x[%d] = %d", i, x[i]);
+	}
+	free(x);
+
+
+return 0;
 }
